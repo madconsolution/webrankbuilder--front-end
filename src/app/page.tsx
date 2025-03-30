caa1e513c8e236a00a114409b60e291e5f622774
md. mahbub alom <mdmahbuba034@gmail.com>
2025-03-29 20:32:00 -0700

    initial setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import HeroSection from "./home/hero/page";
+import IntroductionSection from "./home/introduction/page";
+
+export default function Home() {
+  return (
+    <>
+      {/* Here is hero section of home page */}
+      <HeroSection />
+      {/* Here is introduction section of home page */}
+      <IntroductionSection />
+    </>
+  );
+}
