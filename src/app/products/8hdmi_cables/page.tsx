b2ffeb5fd7c862586053a56d86a63119da2f0877
Mahfuz <aamahfuz.pro@gmail.com>
2025-04-15 22:55:58 +0600

    Products: hdmi8_cables_page

---
+++ src/app/products/8hdmi_cables/page.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import CablesBanner from "@/app/reuseable/cables/banner/page";
+import ResolutionSection from "@/app/reuseable/cables/Resolution/page";
+import SpecificationSection from "@/app/reuseable/cables/Specification/page";
+import CallToActionBanner from "@/app/reuseable/products/join-group/page";
+import Title from "@/app/reuseable/products/title/page";
+
+// Here is specification date
+const specs = [
+    { label: 'Model', value: 'USB C To HDMI' },
+    { label: 'Built In Conversion Chip', value: 'Yes' },
+    { label: 'PVC Jackets', value: 'Yes' },
+    { label: 'Shell Type', value: 'Aluminium' },
+    { label: 'Max Resolution', value: '4k@60Hz' },
+    { label: 'Compatibility', value: 'Smartphone, TV, Projector' },
+  ];
+
+//   Here is resolution data
+
+
+
+  const resolutionData = [
+    {
+      mainImage: '/cables/4hdmi_cables/resolution/cable-1.png',
+      overlayImage: '/cables/4hdmi_cables/resolution/right-1.png',
+      title: 'Stream Visuals From Your Smartphone',
+      descriptionImage: '/cables/4hdmi_cables/resolution/monitor.png',
+    },
+    {
+      mainImage: '/cables/4hdmi_cables/resolution/cable-2.png',
+      title: 'Experience The Brilliance Of',
+      descriptionImage: '/cables/4hdmi_cables/resolution/right-1.png',
+      badges: [
+        '/cables/4hdmi_cables/resolution/right-1.png',
+        '/cables/4hdmi_cables/resolution/right-2.png',
+        '/cables/4hdmi_cables/resolution/right-3.png',
+      ],
+    },
+  ];
+const hdmi8_cables_page = () => {
+  return (
+    <div className="w-full md:w-[1440px] mx-auto">
+   <CablesBanner
+      bgImage="/cables/8hdmi_cables/banner.png"
+      logo1="/logo.png"
+      logo2="/products/IRIS.png"
+      description={[
+        'ATX MID TOWER COMPUTER',
+        'CASE',
+        'THAT HAVE GREAT DESIGN',
+      ]}
+    />
+     
+       {/* Here is first heading title */}
+            <div className="text-xl text-[#FFFFFF]">
+             <Title
+               mainTitle="TORTOX"
+               subTitle=" High Speed HDMI 2.1 8k 60Hz CABLES"
+               content="Tortox high-speed HDMI cable is designed and manufactured for quality, durability & efficiency. The cable helps you to enjoy 8k resolution @60Hz refresh rate for an ultimate visual experience. Moreover, the cable can transfer 18Gbps to 48Gbps which means a 6GB film can be transferred in seconds; users can experience lag-free visual and audio, Additionally, Dynamic HDR and 3D effects further optimize the visual experience. The top configuration is backward compatible with HDMI 2.0b/2.0a/1.4/1.3/1.2/1.1 versions, and supports Dolby Vision and Dolby Sound, offering an exceptional experience. The HDMI cable features connectors coated with 24k gold and an aluminum shell body. Furthermore, the Tortox HDMI Cable is made with oxygen-free copper conductors to enhance both image and sound quality, ensuring no compromise on quality."
+             />
+              </ div>
+        {/* Resolution section */}
+        <ResolutionSection cards={resolutionData} />;
+        {/* Specification section */}
+        <SpecificationSection specifications={specs} />
+           
+          {/* Join Group  */}
+      <CallToActionBanner
+  text="Over 1000's of gamers already using our products."
+  buttonLabel="Join Our Group"
+  buttonHref="/community"
+/>
+
+    </ div>
+  );
+};
+
+
+
+export default hdmi8_cables_page;
