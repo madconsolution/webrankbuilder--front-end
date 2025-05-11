798dadbd13a6b52e32af5dfa50ade6b451bf45cf
Mahfuz <aamahfuz.pro@gmail.com>
2025-05-12 02:37:05 +0600

    Products: updated specifications

--- src/app/products/gs_300_g/page.tsx
+++ src/app/products/gs_300_g/page.tsx
@@ -50,26 +50,32 @@ import SpecificationCheir from "@/app/reuseable/specifications/specification_che
 
 // Here is specification date
 const specs: SpecificationItem[] = [
+
+
   {
     type: 'featured',
     items: [
-      'Aorus fan no need an extra controller for rainbow effect',
-      'Stand out with vibrant RGB lighting',
-      'Custom engineered for superior cooling',
-      'Minimize noise or maximize airflow',
-      'Stay quiet, run cool!',
+      'Tortox GS300-R Series gaming chairs are suitable for those who want the simpliest design with esports feeling.',
+      { label: 'Color  ',  value: ' Grey & Red', highlight: true },
+      { label: 'Frame  ', value: 'Metal', highlight: true },
+      { label: 'PVC Leather', value: 'Chair Cover Material ', highlight: true },
+      { label: 'Armrests ', value: '4D', highlight: true },
+      { label: 'Gas Lift Specification ', value: '80/40, 75mm', highlight: true },
+      { label: 'Base ', value: '2705" Wide Metal Base', highlight: true },
+      { label: 'Casters ', value: '  3" Caster/PU', highlight: true },
     ],
   },
   {
     type: 'dimensions',
     items: [
       'The backrest is adjustable, allowing you to find the perfect angle for maximum comfort during long gaming sessions.',
-      { label: 'Voltage', value: 'DC 12V' },
-      { label: 'Fan Dimensions', value: '120 x 120 x 25mm' },
-      { label: 'Fan Speed', value: '800~2000 R.P.M' },
-      { label: 'Air Flow', value: '18~25.4 CFM' },
-      { label: 'Static Pressure', value: '0.55~1.51 mm-H2O' },
-      { label: 'Noise Level', value: '≤13.5~25.8 dB(A)' },
+      { label: 'Recommended Weight ',  value: ' PVC Leather', highlight: true },
+      { label: 'Recommended Height ', value: ' Up to 6"5"', highlight: true },
+      { label: 'Adjustable Back Angle ', value: '135°', highlight: true },
+      { label: 'Adjustable Headrest ', value: 'Yes', highlight: true },
+      { label: 'Adjustable Lumbar Cushion ', value: 'Yes', highlight: true },
+      { label: 'Items Dimension(L x W x H) ', value: '800 x 800 x 1200mm ', highlight: true },
+      { label: 'Net Weight ', value: '150Kg', highlight: true },
     ],
   },
   {
