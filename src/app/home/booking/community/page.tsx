9a9efe1cbbcc39fb6f1e0d82dadf163134358b05
Mahfuz <aamahfuz.pro@gmail.com>
2025-05-15 02:11:22 +0600

    Home: modified coumunity section

--- src/app/home/booking/community/page.tsx
+++ src/app/home/booking/community/page.tsx
@@ -2,10 +2,10 @@
 
 const CommunitySection = () => {
   return (
-    <div className="relative min-h-[400px] flex items-center justify-center text-center px-4">
+    <div className="relative min-h-[400px] flex items-center justify-center text-center px-4  ">
       {/* Darker Background Image */}
       <div
-        className="absolute inset-0 bg-cover bg-center bg-black bg-opacity-80"
+        className="absolute inset-0 bg-cover bg-center bg-black opacity-100"
         style={{
           backgroundImage:
             "url('https://i.postimg.cc/5y2pv6mX/1690951178-gaming-pc-category-hero-bg-2xl.png')",
@@ -13,13 +13,13 @@ const CommunitySection = () => {
       ></div>
 
       {/* Content Section */}
-      <div className="relative p-8 rounded-lg max-w-5xl text-white z-10">
-        <h1 className="text-3xl font-bold mb-4 text-white">
+      <div className="relative p-8 rounded-lg max-w-5xl text-white z-10   ">
+        <h6 className="text-2xl font-sans">
           Over 1,000 gamers are already using our products.
-        </h1>
-        <h3 className="text-2xl mb-6 text-white">
+        </h6>
+        <h6 className="text-xl">
           Designed with all the goodies you can get, Tortox will become your favorite brand.
-        </h3>
+        </h6>
         <button className="btn border-2 text-white px-6 py-3 rounded-sm text-lg font-semibold transition-transform transform hover:scale-105">
           JOIN OUR GROUP
         </button>
